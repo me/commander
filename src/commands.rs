a814075d4a3aa6bd7bccc273ed88febca7406394
Ivan Pirlik <abmajor7@gmail.com>
2019-02-13 22:06:01 +0000

    Read and show commands

--- src/commands.rs
+++ src/commands.rs
@@ -1,1 +1,47 @@
+use std::ffi::OsString;
+use std::fs::{self, File};
+use std::io::prelude::*;
+use std::io::{self, BufReader};
+use std::path::PathBuf;
+
+use std::sync::mpsc::SyncSender;
+
 pub const EXTENSION: &str = "commands";
+
+pub struct Commands {
+  path: PathBuf,
+}
+
+impl Commands {
+  pub fn new(path: OsString) -> Commands {
+    let path = PathBuf::from(path);
+    Commands { path }
+  }
+
+  pub fn parse(&self, tx: &SyncSender<u8>) {
+    let _ = Commands::parse_dir(&self.path, tx);
+  }
+
+  fn parse_dir(dir: &PathBuf, tx: &SyncSender<u8>) -> io::Result<()> {
+    if dir.is_dir() {
+      for entry in fs::read_dir(dir)? {
+        let entry = entry?;
+        let path = entry.path();
+        if path.is_dir() {
+          let _ = Commands::parse_dir(&path, tx);
+        } else if entry.path().extension().unwrap_or(&OsString::from("")) == EXTENSION {
+          let f = File::open(entry.path());
+          if let Ok(f) = f {
+            let f = BufReader::new(f);
+            for b in f.bytes() {
+              if let Ok(b) = b {
+                let _ = tx.send(b);
+              }
+            }
+          }
+        }
+      }
+    }
+    Ok(())
+  }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,54 @@
 extern crate clap;
 extern crate dirs;
 extern crate failure;
+extern crate futures;
 extern crate serde_derive;
 extern crate skim;
 
+use std::io::BufReader;
+use std::sync::mpsc::sync_channel;
+use std::thread;
+
 use clap::{App, Arg, ArgMatches};
 use failure::Error;
+use iter_read::IterRead;
 
 mod commands;
 mod options;
 mod tldr;
 
-// use skim::{Skim, SkimOptions};
-// use std::default::Default;
-// use std::io::Cursor;
+use skim::{Skim, SkimOptions};
 
 pub const APP_NAME: &str = env!("CARGO_PKG_NAME");
 pub const VERSION: &str = env!("CARGO_PKG_VERSION");
+
+const BUF_SIZE: usize = 10;
 
 pub fn main() {
     let args = get_args();
     let options = options::Options::from_args(&args);
     if options.update_sources {
-        let updater = tldr::Updater::new(options.tldr_url, options.cache_path);
+        let updater = tldr::Updater::new(options.tldr_url, options.cache_path.clone());
         match_result(updater.update());
     }
-    // let options: SkimOptions = SkimOptions::default()
-    //     .height("30%")
-    //     .prompt("Search: ")
-    //     .reverse(true);
+    let (tx, rx) = sync_channel(BUF_SIZE);
+    let commands = commands::Commands::new(options.cache_path);
 
-    // let input = "aaaaa\nbbbb\nccc".to_string();
+    thread::spawn(move || commands.parse(&tx));
+    let options: SkimOptions = SkimOptions::default()
+        .height("30%")
+        .prompt("Search: ")
+        .reverse(true);
 
-    // let selected_items = Skim::run_with(&options, Some(Box::new(Cursor::new(input))))
-    //     .map(|out| out.selected_items)
-    //     .unwrap_or_else(Vec::new);
+    let iter_read = IterRead::new(rx.into_iter());
 
-    // for item in selected_items.iter() {
-    //     print!("{}: {}{}", item.get_index(), item.get_output_text(), "\n");
-    // }
+    let selected_items = Skim::run_with(&options, Some(Box::new(BufReader::new(iter_read))))
+        .map(|out| out.selected_items)
+        .unwrap_or_else(Vec::new);
+
+    for item in selected_items.iter() {
+        print!("{}: {}{}", item.get_index(), item.get_output_text(), "\n");
+    }
 }
 
 fn match_result<T>(res: Result<T, Error>) {
